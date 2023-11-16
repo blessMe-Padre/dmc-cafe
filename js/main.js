3ee0a1a77b15958990f17a1e387218ecc142a7ff
Сергей Субботин <zarodiny@yandex.ru>
2023-11-16 14:34:38 +1000

    init: start

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import { initMenu } from "./module/menu.js";
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('подключен скрипт main.js');
+
+    initMenu();
+
+    // DOMContentLoaded
+});
