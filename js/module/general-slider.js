1217eb9c78e3ead9904acbcc7310beadd44e193f
Сергей Субботин <zarodiny@yandex.ru>
2023-11-25 20:28:36 +1000

    fix: баг фикс

--- js/module/general-slider.js
+++ js/module/general-slider.js
@@ -1,45 +1,48 @@
 export const initGeneralSlider = () => {
-    console.log('initGeneralSlider');
-    console.log(innerWidth)
-    const swiper = new Swiper(".new-items-2", {
-        slidesPerView: 2,
-        spaceBetween: 15,
-        grid: {
-            rows: 2,
-        },
-        navigation: {
-            nextEl: '.next-new--item',
-            prevEl: '.prev-new--item',
-        },
+    const generalSlider = document.querySelector('.new-items-2');
 
-        breakpoints: {
-            320: {
-                grid: {
-                    rows: 2,
-                },
+    if (generalSlider) {
+
+        const swiper = new Swiper(generalSlider, {
+            slidesPerView: 2,
+            spaceBetween: 15,
+            grid: {
+                rows: 2,
             },
-            460: {
-                grid: {
-                    rows: 1,
-                },
-                slidesPerView: 1.5,
-                spaceBetween: 15
-            },
-            767: {
-                slidesPerView: 3,
-                spaceBetween: 15,
-                grid: {
-                    rows: 1,
-                },
+            navigation: {
+                nextEl: '.next-new--item',
+                prevEl: '.prev-new--item',
             },
 
-            1023: {
-                slidesPerView: 4,
-                spaceBetween: 20,
-                grid: {
-                    rows: 1,
+            breakpoints: {
+                320: {
+                    grid: {
+                        rows: 2,
+                    },
                 },
-            },
-        }
-    });
+                460: {
+                    grid: {
+                        rows: 1,
+                    },
+                    slidesPerView: 1.5,
+                    spaceBetween: 15
+                },
+                767: {
+                    slidesPerView: 3,
+                    spaceBetween: 15,
+                    grid: {
+                        rows: 1,
+                    },
+                },
+
+                1023: {
+                    slidesPerView: 4,
+                    spaceBetween: 20,
+                    grid: {
+                        rows: 1,
+                    },
+                },
+            }
+        });
+    }
 }
